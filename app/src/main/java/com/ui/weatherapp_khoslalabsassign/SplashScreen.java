c7f17caf7d89a68422274ec695a9b2314092c5e5
rajat <rajatrrpalankar@gmail.com>
2020-02-11 11:10:43 +0530

    SplashScreen Loading Animation

--- app/src/main/java/com/ui/weatherapp_khoslalabsassign/SplashScreen.java
+++ app/src/main/java/com/ui/weatherapp_khoslalabsassign/SplashScreen.java
@@ -2,13 +2,45 @@ package com.ui.weatherapp_khoslalabsassign;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
 
 public class SplashScreen extends AppCompatActivity {
+    ImageView loading;
+    RotateAnimation rotate;
+
+    Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        loading=(ImageView)findViewById(R.id.loading);
+        handler = new Handler();
+
+        rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        rotate.setDuration(3000);
+        rotate.setInterpolator(new LinearInterpolator());
+
+        loading.startAnimation(rotate);
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+
+                startActivity(i);
+
+                // close this activity
+
+                finish();
+            }
+        },3000);
     }
 }
